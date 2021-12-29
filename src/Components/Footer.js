8bea1d75e97759fb6c86e80d0e1413930ab5de5b
tekkh <tkhorbi@gmail.com>
2021-12-29 23:35:06 +0100

    Edit footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by Tawfik KHORB ♥️</li>
+            <li>Modified with love by Tawfik KHORB <span>♥️</span></li>
            </ul>
 
            <ul className="copyright">
