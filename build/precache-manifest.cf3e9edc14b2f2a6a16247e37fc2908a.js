0f14c58ab31265eed435f9afbb8c4021ec5345e6
tekkh <tkhorbi@gmail.com>
2021-12-29 23:27:27 +0100

    All files

---
+++ build/precache-manifest.cf3e9edc14b2f2a6a16247e37fc2908a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2813a327b6a69aad1420cb8f90f97dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709e43a5d7141c1e84a5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709e43a5d7141c1e84a5",
+    "url": "/static/js/main.7a9568f4.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
